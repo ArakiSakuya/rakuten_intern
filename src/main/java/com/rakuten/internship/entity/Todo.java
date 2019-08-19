fd40fd63453c80fef4fc945b068468b467a75ba9
ArakiSakuya <kiri.0696@gmail.com>
2019-08-19 17:02:33 +0000

    20190819

--- src/main/java/com/rakuten/internship/entity/Todo.java
+++ src/main/java/com/rakuten/internship/entity/Todo.java
@@ -4,8 +4,41 @@ package com.rakuten.internship.entity;
  * このクラスは、タスク管理の対象となるTodoのエンティティーです。
  * エンティティーとして使えるように、コードを記入してください。
  */
-public class Todo {
-    // TODO 必要なコードを記入してください。
-    //test
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Todo {
+    private static final String Generationtype = null;
+	// TODO 必要なコードを記入してください。
+    // test
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer no;
+    private String name;
+    private String description;
+    
+    public Integer getNo(){
+        return no;
+    }
+    public void setNo(Integer no){
+        this.no = no;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    
 }
--- src/main/java/com/rakuten/internship/service/TodoService.java
+++ src/main/java/com/rakuten/internship/service/TodoService.java
@@ -3,6 +3,9 @@ package com.rakuten.internship.service;
 import java.util.List;
 
 import com.rakuten.internship.entity.Todo;
+import com.rakuten.internship.repository.TodoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,14 +17,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class TodoService {
 
     // TODO 必要なメンバーを宣言してください。
+    @Autowired
+    private TodoRepository todoRepository;
 
     public List<Todo> findTodos(){
         // TODO 必要なコードを作成してください。
-        return null;
+        return todoRepository.findAll();
     }
 
     public Todo save(Todo todo) {
         // TODO 必要なコードを作成してください。
-        return null;
+        return todoRepository.save(todo);
     }
 }
